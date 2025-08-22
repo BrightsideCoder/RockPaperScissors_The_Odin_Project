ccbc5f3487245398b35a7593d4e111e2d332b480
Torsten Krüger <torsten.kruger90@gmail.com>
2025-08-22 16:16:49 +0200

    Added function for the user input

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,5 +1,6 @@
 function getComputerChoice(){
-    var computerChoice = Math.floor(Math.random() * 3 + 1);
+    let computerChoice = Math.floor(Math.random() * 3 + 1);
+    
     if (computerChoice == 1) {
         computerChoice = "Paper";
     } else if (computerChoice == 2) {
@@ -10,4 +11,21 @@ function getComputerChoice(){
     return computerChoice;
 }
 
+function getUserChoice() {
+    
+    let userChoice = prompt("Rock, paper or scissors? (1, 2 or 3)");;
+    
+    if (userChoice == 1) {
+        userChoice = "Paper";
+    } else if (userChoice == 2) {
+        userChoice = "Scissors";
+    } else if (userChoice == 3){
+        userChoice = "Rock";
+    } else {
+        console.log("Please choose between 1, 2 or 3");
+    }
+    return userChoice;
+}
+
 console.log(getComputerChoice());
+console.log(getUserChoice());
