ea43aedc855cb012ec6766cd2f7107cf81388d7e
Torsten Krüger <torsten.kruger90@gmail.com>
2025-08-22 21:06:00 +0200

    Added player and computer scoring

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -26,9 +26,12 @@ function playRound(userChoice, computerChoice) {
         console.log("It's a tie!")
     } else if (userChoice == "Paper" && computerChoice == "Rock" || userChoice == "Scissors" && computerChoice == "Paper" || userChoice == "Rock" && computerChoice == "Scissors") {
                 console.log(`User chose: ${userChoice} and the computer chose: ${computerChoice}. User wins!`)
+                playerScore++;
                } else if ( userChoice == "Paper" && computerChoice == "Scissors" || userChoice == "Scissors" && computerChoice == "Rock" || userChoice == "Rock" && computerChoice == "Paper") {
                             console.log(`User chose: ${userChoice} and the computer chose: ${computerChoice}. Computer wins...`)
+                            computerScore++;
                            }
 }
 
 playRound(getUserChoice(), getComputerChoice());
+console.log(`Player score: ${playerScore}\n Computer score: ${computerScore}`);
