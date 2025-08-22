9a650a1622cbb6e0068b40b9986ca0488984b9a9
Torsten Krüger <torsten.kruger90@gmail.com>
2025-08-22 16:07:21 +0200

    Added getComputerChoice function that randomly outputs rock, paper or scissors

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,1 +1,13 @@
-console.log("Hello World");
+function getComputerChoice(){
+    var computerChoice = Math.floor(Math.random() * 3 + 1);
+    if (computerChoice == 1) {
+        computerChoice = "Paper";
+    } else if (computerChoice == 2) {
+        computerChoice = "Scissors";
+    } else {
+        computerChoice = "Rock";
+    }
+    return computerChoice;
+}
+
+console.log(getComputerChoice());
