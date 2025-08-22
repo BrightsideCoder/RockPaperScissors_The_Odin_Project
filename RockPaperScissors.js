912a0f390222b1ca362f69c60605006ab2bcf8d4
Torsten Krüger <torsten.kruger90@gmail.com>
2025-08-22 20:06:40 +0200

    Made the getUserChoice actually make sense... No weird logic

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,6 +1,8 @@
+let playerScore = 0, computerScore = 0;
+
 function getComputerChoice(){
     let computerChoice = Math.floor(Math.random() * 3 + 1);
-    
+
     if (computerChoice == 1) {
         computerChoice = "Paper";
     } else if (computerChoice == 2) {
@@ -13,18 +15,14 @@ function getComputerChoice(){
 
 function getUserChoice() {
     
-    let userChoice = prompt("Rock, paper or scissors? (1, 2 or 3)");;
+    let choice = prompt("Rock, paper or scissors?").toLowerCase();
+    let userChoice = choice.charAt(0).toUpperCase() + choice.slice(1);
     
-    if (userChoice == 1) {
-        userChoice = "Paper";
-    } else if (userChoice == 2) {
-        userChoice = "Scissors";
-    } else if (userChoice == 3){
-        userChoice = "Rock";
-    } else {
-        console.log("Please choose between 1, 2 or 3");
-    }
     return userChoice;
+}
+
+function playRound() {
+
 }
 
 console.log(getComputerChoice());
